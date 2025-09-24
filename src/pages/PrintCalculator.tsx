9245089d5149aadce40760b194bd62706d2a1646
[dyad] <christoforoscosma@gmail.com>
2025-09-24 13:00:16 +0300

    [dyad] Increased bottom padding on PrintCalculator page - wrote 1 file(s)

--- src/pages/PrintCalculator.tsx
+++ src/pages/PrintCalculator.tsx
@@ -125,7 +125,7 @@ const PrintCalculator = () => {
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 p-4 pb-20">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 p-4 pb-44">
       <Card className="w-full max-w-2xl shadow-lg">
         <CardHeader className="relative">
           <CardTitle className="text-3xl font-bold text-center">PrintWise Calculator</CardTitle>
